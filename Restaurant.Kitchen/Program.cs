bcddca8f2a276a319752127d2742d01f6fdb3891
Иван Сафронов <safroniv@mail.ru>
2023-03-25 23:56:42 +0300

    Add project files.

---
+++ Restaurant.Kitchen/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Restaurant.Kitchen
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
